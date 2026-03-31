41b0a185af222eac87ccf843204baefea749a734
alexandrabladag <alexandrabladag@gmail.com>
2026-03-31 17:23:23 +0800

    Add RoleSeeder for staging/production setup

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'manager', 'guard_name' => 'web']);
+        Role::firstOrCreate(['name' => 'client', 'guard_name' => 'web']);
+
+        $this->command->info('✅ Roles created.');
+    }
+}
